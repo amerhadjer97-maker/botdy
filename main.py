8f0f4dca3600f9201650e12f6cbbfc87fbb07caf
amerhadjer97-maker <amerhadjer97@gmail.com>
2025-12-07 02:37:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = os.getenv("7996482415:AAFZh4E-ivoOhRi8s_6Vg2qKvATOhAm54ek")
+TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("البوت اشتغل بنجاح ✔🔥")
+    await update.message.reply_text("🔥 البوت يعمل بنجاح!")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
