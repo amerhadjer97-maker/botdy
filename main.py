65bb8746901071e7ce273357461fd1b85664b1ed
amerhadjer97-maker <amerhadjer97@gmail.com>
2025-12-18 01:14:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
-import os
-import asyncio
-from flask import Flask, request
-from telegram import Update, Bot
+from flask import Flask
+from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("BOT_TOKEN")
+# =====================
+# TOKEN
+# =====================
+TOKEN = "7996482415:AAFhRRnmu7Fr41zkAa9OHuKntWMeqOwqRaI"
 
+# =====================
+# Flask
+# =====================
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Bot is running ✅"
+
+# =====================
+# Telegram Bot
+# =====================
 application = ApplicationBuilder().token(TOKEN).build()
-bot = Bot(TOKEN)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("👋 البوت يعمل بنجاح ✅")
 
 application.add_handler(CommandHandler("start", start))
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    update = Update.de_json(request.get_json(force=True), bot)
-    asyncio.run(application.process_update(update))
-    return "ok", 200
-
-@app.route("/")
-def home():
-    return "Bot is running ✅"
+# =====================
+# Run
+# =====================
+if __name__ == "__main__":
+    application.run_polling()
