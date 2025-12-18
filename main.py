507a7399e2287e7b303807a695055b81153bbae9
amerhadjer97-maker <amerhadjer97@gmail.com>
2025-12-18 01:17:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# =====================
-# TOKEN
-# =====================
 TOKEN = "7996482415:AAFhRRnmu7Fr41zkAa9OHuKntWMeqOwqRaI"
 
-# =====================
-# Flask
-# =====================
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Bot is running ✅"
-
-# =====================
-# Telegram Bot
-# =====================
+# Telegram application
 application = ApplicationBuilder().token(TOKEN).build()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("👋 البوت يعمل بنجاح ✅")
+    await update.message.reply_text("👋 البوت يعمل عبر Webhook ✅")
 
 application.add_handler(CommandHandler("start", start))
 
-# =====================
-# Run
-# =====================
+# Telegram webhook
+@app.route("/webhook", methods=["POST"])
+async def webhook():
+    update = Update.de_json(request.get_json(force=True), application.bot)
+    await application.process_update(update)
+    return "ok"
+
+# Home
+@app.route("/")
+def home():
+    return "Webhook Bot is running ✅"
+
 if __name__ == "__main__":
-    application.run_polling()
+    app.run(host="0.0.0.0", port=10000)
