1d3cb8cd0caa9380df333a035fb349f728d9690f
amerhadjer97-maker <amerhadjer97@gmail.com>
2025-12-18 02:14:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "7996482415:AAFhRRnmu7Fr41zkAa9OHuKntWMeqOwqRaI"
+TOKEN = "7996482415:AAF9fYTkzY3nyzCldCcBsMRoBr6Kwg27YfM"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ البوت يعمل بدون مشاكل")
+    await update.message.reply_text("✅ أخيرًا… البوت يعمل")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
